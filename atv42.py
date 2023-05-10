74740814e74c24fd9f69c9da9d4a1a0bcf27a459
wwwbucha <132013735+wwwbucha@users.noreply.github.com>
2023-05-10 18:28:01 -0300

    42

---
+++ atv42.py
@@ -0,0 +1,5 @@
+real1= float(input("Me diga um numero real: "))
+int1 = int(input("Me diga um numero inteiro: "))
+int2 = int(input("Me diga um numero inteiro: "))
+
+print ("O valor real é %f e os inteiro são %d e %d." % (real1,int1,int2))
