40808ba0a2ae83644d9732e1ce465426ea7fcde9
wwwbucha <132013735+wwwbucha@users.noreply.github.com>
2023-05-12 15:17:24 -0300

    atv

---
+++ atv43.py
@@ -0,0 +1,11 @@
+a=float(input("digite um numero: "))
+b=float(input("digite outro numero: "))
+c=float(input("digite o terceiro numero: "))
+
+a2 = (a*2)*(b/2)
+b2 = (c + (a*3))
+c3 = c**3
+
+print("1= %.2f"% a2)
+print("2= %.2f"% b2)
+print("3= %.2f"% c3)
