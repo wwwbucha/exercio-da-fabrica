5fd15e325beed645f9e824d37432362c1ad5059b
wwwbucha <132013735+wwwbucha@users.noreply.github.com>
2023-05-17 17:41:01 -0300

    attttvs

---
+++ atv_ler numero copy.py
@@ -0,0 +1,15 @@
+a = input("Primeiro Produto: ")
+b = input("Segundo Produto: ")
+c = input("Terceiro Produto: ")
+
+
+a1 = float (input("digite o preço do primeiro produto: "))
+b2 = float (input("digte o preço do segundo produto: "))
+c3 = float (input("digite o preço do terceiro produto: "))
+produto = [a1,b2,c3]
+pt = [a,b,c]
+print (min(pt),end=" ")
+print (min(produto))
+
+
+           
