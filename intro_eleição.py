f85b3be00f4a3d18f3b791e136059777a2c44c9d
wwwbucha <christian03enzo0419@gmail.com>
2023-05-31 14:50:16 -0400

    atvs

--- intro_eleição.py
+++ intro_eleição.py
@@ -5,7 +5,7 @@ total=0
 eleitores=int(input("Quantos eleitores iram votar: "))
 for a in range(eleitores):
     vt = int(input("Candidato 1 \n Candidato 2 \n Candidato 3 \n"))
-    if vt == 1:
+    if   vt == 1:
         cont+=1
     elif vt== 2:
         cont2+=1
---
+++ intro_manoel.py
@@ -0,0 +1,3 @@
+for i in range(1,51):
+    total=i*1.99
+    print(total)
