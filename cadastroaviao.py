bb01a50e470afd625c837c2247cbdbd66c3f07df
wwwbucha <christian03enzo0419@gmail.com>
2023-06-05 16:53:09 -0400

    ATVS

---
+++ cadastroaviao.py
@@ -0,0 +1,40 @@
+import os
+print("================================================")
+print("|                                              |")
+print("|     SEJA BEM VINDO AO CADASTRO DE VIAGENS    |")
+print("|                                              |")
+print("================================================")
+
+
+aa=[]
+bb=[]
+cc=[]
+dd=[]
+ee=[]
+ff=[]
+gg=[]
+
+print("Vamos começar seu cadastro!")
+while True:
+    try:
+        print("Selecione a opção")
+        op=int(input("Digite as seguintes opçoes: \n 1-Cadastro \n 0-sair \n Digite a opção: "))
+        if op==1:  
+    except ValueError:
+            a = str(input("Digite seu nome: "))
+            b = str(input("Digite seu sobrenome: "))
+            c = int(input("Digite seu RG: "))
+            d = int(input("Digite seu CPF: "))
+            e = str(input("Digite seu endereço: "))
+            f = int(input("Digite o seu numero de telefone: "))
+            g = int(input("Digite sua idade: "))
+            os.system("pause")
+            os.system("cls")
+    if op==0:
+         break
+     
+     
+print("tenha um otimo dia ")
+
+
+passa=("VAMOS PARA O CADASTRO DA PASSAGEM")
